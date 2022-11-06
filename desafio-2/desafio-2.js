f78aa67aa06c86e75a65ed1c1b14b03715b8f96c
Del Campo Fabian <fabydelcampo@hotmail.com>
2022-11-06 20:24:02 -0300

    Correccion desafio-2

--- desafio-2/desafio-2.js
+++ desafio-2/desafio-2.js
@@ -41,7 +41,6 @@ class Contenedor {
             const content = await this.getAll()
             if (content.length > 0) {
                 const obj = content.find(obj => obj.id === id)
-                console.log(obj)
                 if (obj) return obj
             }
             throw new Error(`Objecto no encontrado con ID: ${id} en el ARCHIVO: ${this.rutaArchivo}`)
@@ -55,7 +54,6 @@ class Contenedor {
             const content = await this.getAll()
             if (content.length > 0) {
                 const index = content.findIndex(obj => obj.id === id)
-                console.log(index)
                 if (index === -1) {
                     throw new Error(`No se puede eliminar el objeto con ID: ${id} porque no existe en el ARCHIVO: ${this.rutaArchivo}`)
                 } else {
@@ -72,8 +70,6 @@ class Contenedor {
     async deleteAll() {
         try {
             await fs.promises.writeFile(this.rutaArchivo, '')
-            console.log(`Se limpió el ARCHIVO: ${this.rutaArchivo}`)
-
         } catch (error) {
             throw new Error(`Error al limpiar el ARCHIVO: ${this.rutaArchivo} \n\t Más info: ${error.message}`)
         }
@@ -81,86 +77,34 @@ class Contenedor {
 
 }
 
-
-
 const productos = new Contenedor('productos')
 
-
-console.log('------------- EJEMPLO DE EJECUCION CON USO DE setTimeout\n')
-
-console.log('\n------------- PASO 1: SE AGREGA EL PRIMER PRODUCTO: Laptop\n')
-
-
-productos.save({
-	title: 'Laptop Asus',
-	price: 3000,
-	thumbnail: 'https://hiraoka.com.pe/media/catalog/product/cache/a357cb11a228eb6f7f15c0ee1ff203af/1/1/119950-1.jpg'
-})
-	.then(id => console.log(`Objeto guardado con ID: ${id}`))
-	.catch(error => console.log(error.message))
-
-setTimeout(() => {
-	console.log('\n------------- PASO 2: SE AGREGA EL SEGUNDO PRODUCTO: Teclado\n')
-	productos.save({
-		title: 'Teclado',
-		price: 300,
-		thumbnail: 'https://m.media-amazon.com/images/I/61DT+r681TL._AC_SY450_.jpg'
-	}).then(id => console.log(`Objeto guardado con ID: ${id}`))
-		.catch(error => console.log(error.message))
-}, 1000)
-
-setTimeout(() => {
-	console.log('\n------------- PASO 3: SE LISTA TODOS LOS PRODUCTOS\n')
-	productos.getAll()
-		.then(data => console.log(data))
-		.catch(error => console.log(error.message))
-}, 2000)
+const productoUno = {
+    title: 'Laptop Asus',
+    price: 3000,
+    thumbnail: 'https://hiraoka.com.pe/media/catalog/product/cache/a357cb11a228eb6f7f15c0ee1ff203af/1/1/119950-1.jpg'
+}
+const productoDos = {
+    title: 'Teclado',
+    price: 300,
+    thumbnail: 'https://m.media-amazon.com/images/I/61DT+r681TL._AC_SY450_.jpg'
+}
+const productoTres = {
+    title: 'TV',
+    price: 600,
+    thumbnail: 'https://i.blogs.es/800731/captura-de-pantalla-2021-06-07-a-las-1.09.46/original.png'
+}
 
 
-setTimeout(() => {
-	console.log('\n------------- PASO 4: SE ELIMINAN TODOS LOS PRODUCTOS \n')
-	productos.deleteAll()
-		.catch(error => console.log(error.message))
-}, 3000)
 
-setTimeout(() => {
-	console.log('\n------------- PASO 5: SE AGREGA NUEVAMENTE EL PRIMER PRODUCTO: Tv \n')
-	productos.save({
-		title: 'TV',
-		price: 600,
-		thumbnail: 'https://i.blogs.es/800731/captura-de-pantalla-2021-06-07-a-las-1.09.46/original.png'
-	}).then(id => console.log(`Objeto guardado con ID: ${id}`))
-		.catch(error => console.log(error.message))
-}, 4000)
+async function admProductos() {
+    await productos.save(productoDos)
+    await productos.getAll()
+    await productos.deleteAll()
+    await productos.save(productoTres)
+    await productos.save(productoUno)
+    await productos.deleteById(2)
+    await productos.getAll()
+}
+admProductos()
 
-setTimeout(() => {
-	console.log('\n------------- PASO 6: SE AGREGA NUEVAMENTE EL SEGUNDO PRODUCTO: Peluche panda \n')
-	productos.save({
-		title: 'Peluche panda',
-		price: 100,
-		thumbnail: 'https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2019/06/19154143/P146.jpg'
-	}).then(id => console.log(`Objeto guardado con ID: ${id}`))
-		.catch(error => console.log(error.message))
-}, 5000)
-
-setTimeout(() => {
-	console.log('\n------------- PASO 7: SE OBTIENE EL SEGUNDO PRODUCTO: Peluche panda \n')
-	productos.getById(2)
-		.then(data => console.log(data))
-		.catch(error => console.log(error.message))
-}, 6000)
-
-setTimeout(() => {
-	console.log('\n------------- PASO 8: SE ELIMINA EL SEGUNDO PRODUCTO: Peluche panda \n')
-	productos.deleteById(2)
-		.catch(error => console.log(error.message))
-}, 7000)
-
-setTimeout(() => {
-	console.log('\n------------- PASO 9 FINAL: SE LISTA LOS PRODUCTOS RESTANTES \n')
-	productos.getAll()
-		.then(data => console.log(data))
-		.catch(error => console.log(error.message))
-}, 8000) 
- 
-//save(recibe un producto)
