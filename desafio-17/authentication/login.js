51706dfc66be5350eb35d61a96ceb67eff41afe2
Del Campo Fabian <fabydelcampo@hotmail.com>
2023-02-22 23:24:17 -0300

    desafio 17

---
+++ desafio-17/authentication/login.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+const UserModel = require(`../db/models/user`);
+
+const { isValidPassword } = require('../utils/utils');
+
+const login = () => {
+    passport.use('login', new LocalStrategy({
+        //Configuración para obtener todo el req.
+        passReqToCallback: true
+    }, async (req, username, password, done) => {
+        try {
+            const user = await UserModel.findOne({ username });
+            if (!user) {
+                return done(null, false);
+            }
+            if (!isValidPassword(user.password, password)) {
+                return done(null, false);
+            }
+            return done(null, user);
+        }
+        catch (err) {
+            done(err);
+        }
+    }));
+}
+
+module.exports = login;
---
+++ desafio-17/controller/getObjectRandom.js
@@ -0,0 +1,29 @@
+const getObject = () => {
+    let cant = process.env.CANT_BUCLE;
+    if (!cant){
+        cant = 100000000;
+    }
+    let arr = [];
+    let data = {};
+
+   
+    for (let i = 0; i <= 1000; i++) {
+        arr[i] = 0;
+    }
+
+    for (let i = 0; i <= cant; i++) {
+        let numRandom = Math.floor((Math.random() * (1001 - 1) + 1));
+        arr[numRandom]++;
+    }
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        data[i] = {
+            vecesQueAparece: arr[i]
+        }
+    }
+
+    return (data);
+}
+const objectAleatorio = getObject();
+
+process.send(objectAleatorio);
